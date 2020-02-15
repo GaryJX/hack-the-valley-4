79c7b1cbf5d85b589ce9b9104d8cce8952ad7d03
Ryan Sue <Raieen@users.noreply.github.com>
2020-02-14 23:20:54 -0500

    Added firebase admin

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var admin = require("firebase-admin");
+var serviceAccount = require("../serviceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ingest-1fea9.firebaseio.com"
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
