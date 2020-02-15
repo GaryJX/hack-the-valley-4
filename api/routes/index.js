76d5f4d362b9be72d1dfec2f63237f32c4da4bf9
Robert Nichita <robert.nickykitta@gmail.com>
2020-02-15 16:50:22 -0500

    added cors

--- api/routes/index.js
+++ api/routes/index.js
@@ -70,7 +70,8 @@ router.post('/ingest/addArticle', [
 });
 
 router.get('/articles',[
-  check('numArticles').isInt()], function(req,res,next){
+  check('numArticles').isInt(),
+  check('preferredCategories').isString()], function(req,res,next){
     const errors = validationResult(req);
     if(!errors.isEmpty()) return res.status(400).json({errors:errors});
 
